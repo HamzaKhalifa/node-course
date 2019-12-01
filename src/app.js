7e5f2cfbf182a324f38d313df9f4191fb7d69d4f
Hamza <hamza.khalifa@esprit.tn>
2019-12-01 01:34:07 +0100

    Changed port to PORT

--- src/app.js
+++ src/app.js
@@ -82,7 +82,7 @@ app.get('*', (req, res) => {
     });
 });
 
-const PORT = process.env.port || 8000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log('Server running on port ' + PORT);
 });
